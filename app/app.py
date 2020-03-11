3c722d2cb1138f98f0f5f0ad17f734737e50175a
J. Allen Burton <jburton6@my.westga.edu>
2020-03-10 21:28:52 -0400

    Finished implementation of UPDATE endpoint

--- app/app.py
+++ app/app.py
@@ -30,11 +30,43 @@ def add_names():
     return jsonify(result), 201
 
     
-@app.route('/name/<string:name>')
+@app.route('/name/<string:name>', methods=['GET'])
 def get_name_details(name):
     all_names = names.get_all()
     for entry in all_names :
         if entry['name'] == name:
             return jsonify(entry), 200
     return jsonify({'message': 'Error! Name not found'}), 404 
+
+
+@app.route('/name', methods=['PUT'])
+def update_names():
+    request_data = request.get_json()
+    for name_entry in request_data['names'] :
+        if 'name' in name_entry :                           #check to see if the name key is in this JSON 
+            if names.get(name_entry['name']) == None :      #name is not present in dictionary so need to add it
+                result = names.add(name_entry) 
+                name_db.write_names(result) 
+                return jsonify(result), 201
+            else:                                           #name is present in dictionary so needs info update
+                if 'gender' in name_entry :
+                    gender = name_entry['gender'] 
+                else:
+                    gender = names.get(name_entry['name'])['gender']
+                if 'ranking' in name_entry :
+                    ranking = name_entry['ranking']
+                else:
+                    ranking = names.get(name_entry['name'])['ranking']
+                names.delete(name_entry['name'])    
+                new_entry = {'name': name_entry['name'], 'gender': gender, 'ranking': ranking}
+                result = names.add(new_entry)
+                name_db.write_names(result)
+                return jsonify(result), 200
+        else:
+            return jsonify({'message': 'Error! Missing name in sent data'}), 400 
+        
+        
+        
+
+
 
