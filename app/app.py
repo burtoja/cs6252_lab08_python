ccca217e118bf461f83a6551042c0aefc3c2ccc4
J. Allen Burton <jburton6@my.westga.edu>
2020-03-13 17:52:25 -0400

    Implement UPDATE allowing for extra or missing fields.

--- app/app.py
+++ app/app.py
@@ -32,8 +32,8 @@ def get_name_details(name):
 def add_names():
     request_data = request.get_json()
     for name_entry in request_data['names'] :
-        if 'name' in name_entry :                           #check to see if the name key is in this JSON 
-            if names.get(name_entry['name']) == None :      #name is not present in dictionary so need to add it
+        if 'name' in name_entry :                           #check to see if the name key is in this JSON from POST 
+            if names.get(name_entry['name']) == None :      #name is not present in local dictionary so need to add it
                 result = names.add(name_entry) 
                 name_db.write_names(result) 
                 return jsonify(result), 201
@@ -45,27 +45,25 @@ def add_names():
     
 @app.route('/name', methods=['PUT'])
 def update_names():
-    request_data = request.get_json()
-    for name_entry in request_data['names'] :
-        if 'name' in name_entry :                           #check to see if the name key is in this JSON 
-            if names.get(name_entry['name']) == None :      #name is not present in dictionary so need to add it
-                result = names.add(name_entry) 
-                name_db.write_names(result) 
-                return jsonify(result), 201
-            else :                                           #name is present in dictionary so needs info update
-                if 'gender' in name_entry :
-                    gender = name_entry['gender'] 
-                else :
-                    gender = names.get(name_entry['name'])['gender']
-                if 'ranking' in name_entry :
-                    ranking = name_entry['ranking']
-                else :
-                    ranking = names.get(name_entry['name'])['ranking']
-                names.delete(name_entry['name'])    
-                new_entry = {'name': name_entry['name'], 'gender': gender, 'ranking': ranking}
-                result = names.add(new_entry)
-                name_db.write_names(result)
-                return jsonify(result), 200
+    post_request_data = request.get_json()
+    for post_name_entry in post_request_data['names'] :
+        if 'name' in post_name_entry :                           #check to see if the name key is in this JSON from POST
+            local_entry = names.get(post_name_entry['name'])        #dictionary
+            if local_entry == None :                             #name is not present in dictionary so need to add it
+                local_result = names.add(post_name_entry) 
+                name_db.write_names(local_result) 
+                return jsonify(local_result), 201
+            else :                                          #name is present in dictionary so needs info update
+                post_keys = []
+                for post_key in post_name_entry :
+                    post_keys.append(post_key)
+                for local_key in local_entry :
+                    if local_key not in post_keys :
+                        post_name_entry.update( {local_key : local_entry[local_key]} )
+                names.delete(post_name_entry['name'])  
+                local_result = names.add(post_name_entry)
+                name_db.write_names(post_name_entry)
+                return jsonify(post_name_entry), 200
         else :
             return jsonify({'message': 'Error! Missing name in sent data'}), 400 
 
@@ -78,4 +76,7 @@ def delete_name(name):
         else:
             names.delete(name) 
             return jsonify(name_info), 200
+        
+    
+        
         
