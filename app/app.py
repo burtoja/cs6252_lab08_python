8888cb96db1605582998e6bb056b9d2a1b9a7e09
J. Allen Burton <jburton6@my.westga.edu>
2020-03-13 21:13:01 -0400

    Fixed so POST and PUT receive single payload rather than possible multiple payloads at once

--- app/app.py
+++ app/app.py
@@ -19,7 +19,7 @@ def get_names():
     return json.dumps({"names": all_names}), 200
 
 
-@app.route('/name/<string:name>', methods=['GET'])
+@app.route('/name/<name>', methods=['GET'])
 def get_name_details(name):
     all_names = names.get_all()
     for entry in all_names :
@@ -31,44 +31,42 @@ def get_name_details(name):
 @app.route('/name', methods=['POST'])
 def add_names():
     post_request_data = request.get_json()
-    for post_name_entry in post_request_data['names'] :
-        if 'name' in post_name_entry :                            
-            if names.get(post_name_entry['name']) == None :     
-                result = names.add(post_name_entry) 
-                name_db.write_names(result) 
-                return jsonify(result), 201
-            else :                                           
-                return jsonify({'message': 'Error! Name already exists.  No changes made.'}), 400
-        else :
-            return jsonify({'message': 'Error! Missing name in sent data'}), 400 
+    if 'name' in post_request_data :                           
+        if names.get(post_request_data['name']) == None :     
+            result = names.add(post_request_data) 
+            name_db.write_names(result) 
+            return jsonify(result), 201
+        else :                                           
+            return jsonify({'message': 'Error! Name already exists.  No changes made.'}), 400
+    else :
+        return jsonify({'message': 'Error! Missing name in sent data'}), 400 
 
     
 @app.route('/name', methods=['PUT'])
 def update_names():
     post_request_data = request.get_json()
-    for post_name_entry in post_request_data['names'] :
-        if 'name' in post_name_entry :                           
-            local_entry = names.get(post_name_entry['name'])        
-            if local_entry == None :                             
-                local_result = names.add(post_name_entry) 
-                name_db.write_names(local_result) 
-                return jsonify(local_result), 201
-            else :                                          
-                post_keys = []
-                for post_key in post_name_entry :
-                    post_keys.append(post_key)
-                for local_key in local_entry :
-                    if local_key not in post_keys :
-                        post_name_entry.update( {local_key : local_entry[local_key]} )
-                names.delete(post_name_entry['name'])  
-                local_result = names.add(post_name_entry)
-                name_db.write_names(post_name_entry)
-                return jsonify(post_name_entry), 200
-        else :
-            return jsonify({'message': 'Error! Missing name in sent data'}), 400 
+    if 'name' in post_request_data :                           
+        local_entry = names.get(post_request_data['name'])        
+        if local_entry == None :                             
+            local_result = names.add(post_request_data) 
+            name_db.write_names(local_result) 
+            return jsonify(local_result), 201
+        else :                                          
+            post_keys = []
+            for post_key in post_request_data :
+                post_keys.append(post_key)
+            for local_key in local_entry :
+                if local_key not in post_keys :
+                    post_request_data.update( {local_key : local_entry[local_key]} )
+            names.delete(post_request_data['name'])  
+            local_result = names.add(post_request_data)
+            name_db.write_names(post_request_data)
+            return jsonify(post_request_data), 200
+    else :
+        return jsonify({'message': 'Error! Missing name in sent data'}), 400 
 
 
-@app.route('/name/<string:name>', methods=['DELETE'])
+@app.route('/name/<name>', methods=['DELETE'])
 def delete_name(name):        
         name_info = names.get(name)
         if name_info == None :
@@ -76,4 +74,7 @@ def delete_name(name):
         else:
             names.delete(name) 
             return jsonify(name_info), 200
+        
+        
+        
         
